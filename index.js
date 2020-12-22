544577a276a73d9f39406ed372ecd322fa00706a
strood <stroodz19@gmail.com>
2020-12-22 15:28:41 -0700

    import methods

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ import url from 'url';
 import formidable from 'formidable';
 
 const uploadFormDetails = (req, res) => {
-  let form = new IncomingForm();
+  let form = new formidable.IncomingForm();
   form.parse(req, (err, fields, files) => {
     let loggedMsg = `
     =================
     Name: ${fields.name},
     Email: ${fields.email},
     Message: ${fields.message}`;
-    appendFile('siteMessages.txt', loggedMsg, (err) => {
+    fs.appendFile('siteMessages.txt', loggedMsg, (err) => {
       if (err) {
         res.writeHead('400', { 'Content-Tye': 'text/html' });
         res.write(
@@ -32,33 +32,37 @@ const uploadFormDetails = (req, res) => {
   });
 };
 
-createServer((req, res) => {
-  // Parse url
-  let q = parse(req.url, true);
-  //Redirect '/' to index
-  if (q.pathname === '/') {
-    q.pathname = '/index';
-  }
-  //Check if file upload, else its file request
-  if (q.pathname === '/formupload') {
-    uploadFormDetails(req, res);
-  } else {
-    let filename = './pages' + q.pathname + '.html';
-    //Read file
-    readFile(filename, (err, data) => {
-      if (err) {
-        // grab, read, and respond w/ 404 page
-        readFile('./pages/404.html', (err, data2) => {
-          res.writeHead('404', { 'Content-Type': 'text/html' });
-          res.write(data2);
+http
+  .createServer((req, res) => {
+    // Parse url
+    let q = url.parse(req.url, true);
+
+    //Redirect '/' to index
+    if (q.pathname === '/') {
+      q.pathname = '/index';
+    }
+
+    //Check if file upload, else its file request
+    if (q.pathname === '/formupload') {
+      uploadFormDetails(req, res);
+    } else {
+      let filename = './pages' + q.pathname + '.html';
+      //Read file
+      fs.readFile(filename, (err, data) => {
+        if (err) {
+          // grab, read, and respond w/ 404 page
+          fs.readFile('./pages/404.html', (err, data2) => {
+            res.writeHead('404', { 'Content-Type': 'text/html' });
+            res.write(data2);
+            return res.end();
+          });
+        } else {
+          // Respond w/ requested page
+          res.writeHead('200', { 'Content-Type': 'text/html' });
+          res.write(data);
           return res.end();
-        });
-      } else {
-        // Respond w/ requested page
-        res.writeHead('200', { 'Content-Type': 'text/html' });
-        res.write(data);
-        return res.end();
-      }
-    });
-  }
-}).listen(8080);
+        }
+      });
+    }
+  })
+  .listen(8080);
