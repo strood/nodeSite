0e7116afdbc444f83aa011ba9c36743625c23191
strood <stroodz19@gmail.com>
2020-12-22 14:49:08 -0700

    change imports to work on repl

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const http = require('http');
-const fs = require('fs');
-const url = require('url');
-const formidable = require('formidable');
+import http from 'http';
+import fs from 'fs';
+import url from 'url';
+import formidable from 'formidable';
 
 const uploadFormDetails = (req, res) => {
-  let form = new formidable.IncomingForm();
+  let form = new IncomingForm();
   form.parse(req, (err, fields, files) => {
     let loggedMsg = `
     =================
     Name: ${fields.name},
     Email: ${fields.email},
     Message: ${fields.message}`;
-    fs.appendFile('siteMessages.txt', loggedMsg, (err) => {
+    appendFile('siteMessages.txt', loggedMsg, (err) => {
       if (err) {
         res.writeHead('400', { 'Content-Tye': 'text/html' });
         res.write(
@@ -32,35 +32,33 @@ const uploadFormDetails = (req, res) => {
   });
 };
 
-http
-  .createServer((req, res) => {
-    // Parse url
-    let q = url.parse(req.url, true);
-    //Redirect '/' to index
-    if (q.pathname === '/') {
-      q.pathname = '/index';
-    }
-    //Check if file upload, else its file request
-    if (q.pathname === '/formupload') {
-      uploadFormDetails(req, res);
-    } else {
-      let filename = './pages' + q.pathname + '.html';
-      //Read file
-      fs.readFile(filename, (err, data) => {
-        if (err) {
-          // grab, read, and respond w/ 404 page
-          fs.readFile('./pages/404.html', (err, data2) => {
-            res.writeHead('404', { 'Content-Type': 'text/html' });
-            res.write(data2);
-            return res.end();
-          });
-        } else {
-          // Respond w/ requested page
-          res.writeHead('200', { 'Content-Type': 'text/html' });
-          res.write(data);
+createServer((req, res) => {
+  // Parse url
+  let q = parse(req.url, true);
+  //Redirect '/' to index
+  if (q.pathname === '/') {
+    q.pathname = '/index';
+  }
+  //Check if file upload, else its file request
+  if (q.pathname === '/formupload') {
+    uploadFormDetails(req, res);
+  } else {
+    let filename = './pages' + q.pathname + '.html';
+    //Read file
+    readFile(filename, (err, data) => {
+      if (err) {
+        // grab, read, and respond w/ 404 page
+        readFile('./pages/404.html', (err, data2) => {
+          res.writeHead('404', { 'Content-Type': 'text/html' });
+          res.write(data2);
           return res.end();
-        }
-      });
-    }
-  })
-  .listen(8080);
+        });
+      } else {
+        // Respond w/ requested page
+        res.writeHead('200', { 'Content-Type': 'text/html' });
+        res.write(data);
+        return res.end();
+      }
+    });
+  }
+}).listen(8080);
